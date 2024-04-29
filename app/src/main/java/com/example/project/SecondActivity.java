e704a01b79c23b81617e550abe6df9bd80c3076d
c22isako <c22isako@student.his.se>
2024-04-29 09:46:03 +0200

    Cleaned SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,14 +1,9 @@
 package com.example.project;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class SecondActivity extends AppCompatActivity {
 
